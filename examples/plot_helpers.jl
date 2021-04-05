351ea5ac4531de8307788d0a8638e55b2f3180e2
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-04-05 15:06:50 -0400

    moving out plotting functions and adding some more plots

---
+++ examples/plot_helpers.jl
@@ -0,0 +1,33 @@
+function GLOM_plot(fig_loc, plot_xs, obs_xs, feat, err, GLOM_feat, GLOM_err, label, label_GLOM)
+    plt = scatter(obs_xs, feat; yerror=err, label=label)
+    plot!(plt, plot_xs, GLOM_feat; ribbons=GLOM_err, fillalpha=0.3, label=label_GLOM)
+    png(plt, fig_loc)
+end
+function GLOM_ind_plots(name_prefix, plot_xs, obs_xs, rvs_and_inds, rvs_and_inds_err, GLOM_at_plot_xs, GLOM_err_at_plot_xs)
+    for i in 2:length(rvs_and_inds)
+        GLOM_plot(fig_dir * name_prefix * "ind$(i-1)", plot_xs, obs_xs, rvs_and_inds[i], rvs_and_inds_err[i], GLOM_at_plot_xs[i], GLOM_err_at_plot_xs[i], "obs ind $(i-1)", "GLOM ind $(i-1)")
+    end
+end
+function GLOM_plots(name_prefix, plot_xs, obs_xs, rvs_and_inds, rvs_and_inds_err, GLOM_at_plot_xs, GLOM_err_at_plot_xs)
+    GLOM_plot(fig_dir * name_prefix * "rv", plot_xs, obs_xs, rvs_and_inds[1], rvs_and_inds_err[1], GLOM_at_plot_xs[1], GLOM_err_at_plot_xs[1], "obs RVs", "GLOM RVs")
+    GLOM_ind_plots(name_prefix, plot_xs, obs_xs, rvs_and_inds, rvs_and_inds_err, GLOM_at_plot_xs, GLOM_err_at_plot_xs)
+end
+function GLOM_plots(name_prefix, plot_xs, obs_xs, rvs_and_inds, rvs_and_inds_err, GLOM_at_plot_xs, GLOM_err_at_plot_xs, rvs_np)
+    GLOM_plot(fig_dir * name_prefix * "rv_nokep", plot_xs, obs_xs, rvs_np, rvs_and_inds_err[1], GLOM_at_plot_xs[1], GLOM_err_at_plot_xs[1], "obs-Kep RVs", "GLOM RVs")
+    GLOM_at_plot_xs[1] += ustrip.(current_ks.(plot_xs.*u"d"))
+    GLOM_plot(fig_dir * name_prefix * "rv", plot_xs, obs_xs, rvs_and_inds[1], rvs_and_inds_err[1], GLOM_at_plot_xs[1], GLOM_err_at_plot_xs[1], "obs RVs", "GLOM+Kep RVs")
+    GLOM_ind_plots(name_prefix, plot_xs, obs_xs, rvs_and_inds, rvs_and_inds_err, GLOM_at_plot_xs, GLOM_err_at_plot_xs)
+end
+function plot_helper(prefix::String, ks::GLOM_RV.KeplerSignal, fit_total_hyperparameters)
+    GLOM_at_plot_xs, GLOM_err_at_plot_xs, GLOM_at_obs_xs = GLOM_RV.GLOM_posteriors(glo, plot_xs, fit_total_hyperparameters; y_obs = GLOM_RV.remove_kepler(glo_rv, current_ks))
+    rvs_np = GLOM_RV.remove_kepler(GLOM_RV.get_rv(glo_rv), glo_rv.time, current_ks)
+    GLOM_plots(prefix, plot_xs, obs_xs, rvs_and_inds, rvs_and_inds_err, GLOM_at_plot_xs, GLOM_err_at_plot_xs, rvs_np)
+    plot_kep_xs = collect(LinRange(0, ustrip(best_period), 1000))
+    scatter(remainder(glo.x_obs, ustrip(best_period)), ustrip.(GLOM_RV.get_rv(glo_rv)) - GLOM_at_obs_xs[1]; yerror=ustrip.(GLOM_RV.get_rv_noise(glo_rv)), label="data")
+    plot!(plot_kep_xs, ustrip.(current_ks.(plot_kep_xs.*u"d")); label="kep")
+    png(fig_dir * prefix * "rv_phase")
+end
+function plot_helper(prefix::String, fit_total_hyperparameters)
+    GLOM_at_plot_xs, GLOM_err_at_plot_xs, GLOM_at_obs_xs = GLOM_RV.GLOM_posteriors(glo, plot_xs, fit_total_hyperparameters)
+    GLOM_plots(prefix, plot_xs, obs_xs, rvs_and_inds, rvs_and_inds_err, GLOM_at_plot_xs, GLOM_err_at_plot_xs)
+end
